f56aeff4023ebac736b391c9714b66c99769ee4a
YASH SHAH <coder@YASHs-MacBook-Air.local>
2025-12-06 02:14:37 +0530

    build error fix

--- app/search/page.js
+++ app/search/page.js
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import SearchClient from './SearchClient';
 
 export const metadata = {
@@ -6,5 +7,9 @@ export const metadata = {
 };
 
 export default function SearchPage() {
-    return <SearchClient />;
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <SearchClient />
+        </Suspense>
+    );
 }
